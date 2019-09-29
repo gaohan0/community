dbf276db2cde5389feb40a415f3f2eaa857d2f10
gaohan <1135494872@qq.com>
2019-09-29 13:29:28 +0800

    init repo

---
+++ src/main/java/xyz/hangao/community/community/controller/HelloController.java
@@ -0,0 +1,18 @@
+package xyz.hangao.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created By gh
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
